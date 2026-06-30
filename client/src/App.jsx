fb6096880afecb579438270788b5625078d94173
Blue0ne <bahaamaher996@gmail.com>
2026-06-30 12:34:39 +0300

    implement sending messages from ui and initialize POST method

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { Children } from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 
 function App() {
@@ -8,7 +11,52 @@ function App() {
       .catch((err) => console.error(err));
   }, []);
 
-  return <h1>Chat App</h1>;
+    const [messages, setMessages] = useState([]);
+    useEffect(() => {
+      fetch("http://localhost:3000/messages")
+      .then((res) => res.json())
+      .then((data) => {
+        setMessages(data);
+      })
+    }, [])
+  
+    const [text, setText] = useState("");
+    const sendMessage = () => {
+      if (!text.trim) return;
+
+      fetch("http://localhost:3000/messages", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({text}),
+      })
+      .then((res) => res.json())
+      .then((newMessage) => {
+        setMessages((prev) => [...prev, newMessage]);
+        setText("");
+      });
+    };
+
+  return (
+    <div>
+      <h1>Chat App</h1>
+      {messages.map((data) => <p key={data.id}>{data.text} </p>)}
+
+      <input
+      value={text}
+      onChange={(e) => setText(e.target.value)}
+      placeholder="Type a message..."
+      onKeyDown={ (e) => {
+        if (e.key === "Enter") {
+          sendMessage();
+      }}
+    }
+    />
+
+    <button onClick={sendMessage}>Send</button>
+    </div>
+  );
 }
 
 export default App;
